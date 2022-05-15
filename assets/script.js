3345d343ec0bc5c2117bcc2a6adacf1c4130c59d
gofoward-z <zlepeng.ucf@gmail.com>
2022-05-15 11:45:56 -0400

    define set of question

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+//define a set of questions
+const questions =[
+    {
+        question:"Inside which HTML element do wo put the JavaScript?",
+        choices:["a.<js>", "b,<javascript>","c.<scripting>","d.<script>"],
+        answer:"d.<script>"
+    },
+    {
+        question:"string values must be enclosed within____ when being assigned to variables.",
+        choices:["a.commas","b.curly brackets", "c.quotes","d.parenthesis"],
+        answer:"c.quotes"
+    },
+    {
+        question:"arrays in JavaScript can be used to store_____.",
+        choices:["a. numbers and strings","b.other arrays","c.booleans","d.all of the above"],
+        answer:"b.other arrays"
+    },
+    {
+        question:"commonly uesd data types DO NOT include:",
+        choices:["a.strings","b.booleans","c.alerts","d.numbers"],
+        answer:"c.alerts"
+    },
+    {
+        question:"how do you creat a function in javascript",
+        choices:["a.function=MyFunction","b.function MyFunction()","c.function:myFunction()","d.createMyFunction()"],
+        answer:"b.function myFunction()"
+    }
+
+]
